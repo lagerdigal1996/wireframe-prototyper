05dff7fbe5403e2223497cfcaaf7199d5cb22d06
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-09-27 07:11:59 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to Wireframe Prototyper</h1>
+            <p>Create your wireframes and prototypes!</p>
+        </div>
+    );
+}
+
+export default App;
